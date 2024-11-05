15ac7f5b5e9517884c8d8e0d8853b08a660c00d6
krish <krishjhawalta@gmail.com>
2024-11-05 21:26:37 +0530

    change a bit

---
+++ practice_for_c.c
@@ -0,0 +1,28 @@
+//
+// Created by krish on 5/11/24.
+//
+#include <stdio.h>
+int main() {
+// program for sum of n no. of natural numbers by taking user input
+  /*  int n;
+  int sum = 0;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  sum = n*(n+1)/2;
+  printf("The sum of the given numbers is %d", sum);
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+  }
+
