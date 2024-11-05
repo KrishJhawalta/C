baac9babafeecfd7baef96796bf84a6eab88fe11
krish <krishjhawalta@gmail.com>
2024-11-05 20:05:32 +0530

    hello every

---
+++ fact.c
@@ -0,0 +1,50 @@
+//
+// Created by krish on 5/11/24.
+//
+#include <stdio.h>
+int main() {
+  /* q1 write a program that takes an input and calculate the factorial
+    int n;
+    printf("Factorial calculator: ");
+    scanf("%d", & n);
+
+    int factorial = 1;
+    for(int i = 1; i <= n; i++){
+      factorial *= i;
+
+    }printf("The factorial is: %d", factorial);*/
+/*
+// program that takes input and calculate the sum of first n natural no.
+int n;
+  int sum;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  sum = n * (n + 1) / 2;
+  printf("The %d number is:%d",n,sum);
+  return 0;
+
+*/
+
+  // program to takes input and prints the value from1 /*
+  /* int num;
+  printf("Enter number: ");
+  scanf("%d", &num);
+  for(int i = 1; i<=num; i++) {
+    printf("%d\n", i);
+  }
+*/
+    // program to takes input and print the multiplicatoin table of given input
+  int n;
+
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  for(int i = 1; i<=10; i++) {
+    printf("\n%d X %d =%d", n, i, n*i);
+  }
+
+
+
+
+
+
+}
