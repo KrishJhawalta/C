86c551f7c3fe1307494c9f62008e5583bde27570
krish <krishjhawalta@gmail.com>
2024-11-05 22:08:58 +0530

    updated

--- practice_for_c.c
+++ practice_for_c.c
@@ -13,8 +13,41 @@ int main() {
 
     */
 
+  // program that takes input and prints value
+  /*int a;
+  printf("enter a number: ");
+  scanf("%d", &a);
+  for(int i = 1; i<=a; i++) {
+    printf("\n%d ", i);
+  }*/
 
+// program that takes input and calculate the factorial
+  // int n;
+  // int factorial = 1;
+  // printf("Enter your number: ");
+  //   scanf("%d", &n);
+  // for (int i = 1; i<=n; i++) {
+  //   factorial *= i;
+  // }printf("The factorial of %d is: %d",n,factorial);
+  //
 
+// program that print multiplication table vis input
+  // int a;
+  // printf("Enter a number: ");
+  // scanf("%d", &a);
+  // for(int i = 1; i<=10; i++) {
+  //   printf("\n%d X %d = %d",a, i,a*i);
+  // }
+/*
+ * program to print a number reverse
+  int a, rev =0;
+  printf("Enter a number: ");
+  scanf("%d", &a);
+  while(a != 0) {
+    int digit = n%10;
+    rev = rev * 10 + digit, a /=0;
+  }printf("reverse number: %d\n", rev);
+*/
 
 
 
